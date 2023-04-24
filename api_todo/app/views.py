f30897c62a385fae77b716ec0d49d6d923d672ac
WilliamJSS <williamsena088@gmail.com>
2023-04-23 23:29:31 -0300

    Add view and serializer

--- api_todo/app/views.py
+++ api_todo/app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from .models import Todo
+from .serializers import TodoSerializer
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def get(request):
+    todo = Todo.objects.all()
+    serializer = TodoSerializer(todo, many=True)
+    return Response(serializer.data)
